362157e1bf66fb0f0b60d44f380b17d411e5f2af
isk-bokang <bo-kang@iskra.world>
2024-05-12 17:56:28 +0900

    feat: add send string message method

--- modules/webflux-websocket-coroutine/src/main/kotlin/app/boboc/util/WebSocketSessionUtil.kt
+++ modules/webflux-websocket-coroutine/src/main/kotlin/app/boboc/util/WebSocketSessionUtil.kt
@@ -1,5 +1,6 @@
 package app.boboc.util
 
+import app.boboc.util.WebSocketSessionUtil.sendMessage
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import kotlinx.coroutines.reactor.awaitSingleOrNull
@@ -13,6 +14,13 @@ object WebSocketSessionUtil {
         val session = this
         session.send(mono {
             session.textMessage(objectMapper.writeValueAsString(message ?: ""))
+        }).awaitSingleOrNull()
+    }
+
+    suspend fun WebSocketSession.sendStringMessage(message: String?){
+        val session = this
+        session.send(mono {
+            session.textMessage(message ?: "null")
         }).awaitSingleOrNull()
     }
 }
