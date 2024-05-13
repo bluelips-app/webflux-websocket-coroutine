fb4c25f2b50d7c7a911ef5feba5517d837996472
isk-bokang <bo-kang@iskra.world>
2024-05-13 23:42:29 +0900

    minor: modify version

--- buildSrc/src/main/kotlin/app/boboc/Deps.kt
+++ buildSrc/src/main/kotlin/app/boboc/Deps.kt
@@ -1,7 +1,7 @@
 package app.boboc
 
 object Deps {
-    const val websocketCoroutineVersion = "0.0.4"
+    const val websocketCoroutineVersion = "0.0.5"
     const val groupName = "app.boboc"
     const val springBootVersion = "3.1.11"
 }
